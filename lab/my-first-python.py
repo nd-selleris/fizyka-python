9ca578b6963f1f33b25094c2d0a7278480c4c980
Nikita Daniszewski <nd@selleris.com>
2026-03-15 09:40:03 +0100

    Aktualizacja struktury

--- lab/my-first-python.py
+++ lab/my-first-python.py
@@ -10,31 +10,21 @@ def oblicz_predkosc_sriednia(droga,czas):
 
 predkosc=oblicz_predkosc_sriednia(droga,czas)
 
-
-
-
-
 if predkosc > 50:
     print('Samochod jedzie szybko')
 else: print('Samochod jedzie wolno')
 
-
-
 sekunda=1
 sekund_w_godzinie=3600
-
 
 def odleglosc_za_czas_w_sekundach(predkosc,sekund_w_godzinie,sekunda):
     odleglosc=(predkosc/sekund_w_godzinie)*sekunda
     odleglosc_rounded=round(odleglosc,2)
     return odleglosc_rounded
 
-
 while sekunda<=5:
     print(sekunda,odleglosc_za_czas_w_sekundach(predkosc,sekund_w_godzinie,sekunda),'km')
     sekunda=sekunda + 1
- 
-
 
 
 
